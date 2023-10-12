a63f490b80429ac310e0d7be33c040a484c02437
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-12 18:56:22 -0400

    updated urls

--- glasses/glasses/urls.py
+++ glasses/glasses/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('criptastic.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', RedirectView.as_view(url='movies/', permanent=True), name='index'),
 ]
