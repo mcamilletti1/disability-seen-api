844fbb399042cb99ff0709598105a312e4ba2d78
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-12 15:50:34 -0400

    updated views

--- glasses/criptastic/views.py
+++ glasses/criptastic/views.py
@@ -1,30 +1,31 @@
-from rest_framework import generics, permissions
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Movie, Cast, Review
 from .serializers import MovieSerializer, CastSerializer, ReviewSerializer
 
-class MovieListCreate(generics.ListCreateAPIView):
+class MovieListCreate(ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
-class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
+class MovieDetail(RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-class CastListCreate(generics.ListCreateAPIView):
+class CastListCreate(ListCreateAPIView):
     queryset = Cast.objects.all()
     serializer_class = CastSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
-class CastDetail(generics.RetrieveUpdateDestroyAPIView):
+class CastDetail(RetrieveUpdateDestroyAPIView):
     queryset = Cast.objects.all()
     serializer_class = CastSerializer
 
-class ReviewListCreate(generics.ListCreateAPIView):
+class ReviewListCreate(ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
-class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+class ReviewDetail(RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
