2a376dda8fd4e3bbe1edf3f16e0c5a78d1180698
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-12 15:42:59 -0400

    updated settings and urls

--- glasses/glasses/settings.py
+++ glasses/glasses/settings.py
@@ -47,8 +47,7 @@ AUTHENTICATION_BACKENDS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
 }
 
