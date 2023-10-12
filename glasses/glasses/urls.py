41c1fd3d31104a68fbd2d80c06ac64b3c30fdb14
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-12 19:06:03 -0400

    fixed error in urls

--- glasses/glasses/urls.py
+++ glasses/glasses/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('disabilityscene.urls')),
+    path('', include('criptastic.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
