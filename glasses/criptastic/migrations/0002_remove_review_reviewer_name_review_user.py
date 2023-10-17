89a2c6f498399e95dc8c5b9bd35ef129336ae08b
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-17 12:05:56 -0400

    edited migration file

--- glasses/criptastic/migrations/0002_remove_review_reviewer_name_review_user.py
+++ glasses/criptastic/migrations/0002_remove_review_reviewer_name_review_user.py
@@ -13,10 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='review',
-            name='reviewer_name',
-        ),
         migrations.AddField(
             model_name='review',
             name='user',
