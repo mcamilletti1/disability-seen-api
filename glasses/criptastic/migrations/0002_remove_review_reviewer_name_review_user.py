0f597157b9d10d5c2880abcd754c629d257270ed
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-17 11:59:37 -0400

    fixed error in migration file

--- glasses/criptastic/migrations/0002_remove_review_reviewer_name_review_user.py
+++ glasses/criptastic/migrations/0002_remove_review_reviewer_name_review_user.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='review',
             name='user',
-            field=models.ForeignKey(default='Anonymous', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
